ac9cd5d344517c162a91d3d472503831f478063a
Mateusz Skulimowski <skulimowski.mateusz98@gmail.com>
2022-04-03 20:02:23 +0200

    configuration firebase and lowgular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'todoapp-36d5d',
+    appId: '1:760172319480:web:92da5771ed44ba6844210d',
+    storageBucket: 'todoapp-36d5d.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyB7lawDA7paBj5OpcY42BYicXSPSVQifqE',
+    authDomain: 'todoapp-36d5d.firebaseapp.com',
+    messagingSenderId: '760172319480',
+    measurementId: 'G-60X20TR04J',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'todoapp-36d5d',
+    appId: '1:760172319480:web:92da5771ed44ba6844210d',
+    storageBucket: 'todoapp-36d5d.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyB7lawDA7paBj5OpcY42BYicXSPSVQifqE',
+    authDomain: 'todoapp-36d5d.firebaseapp.com',
+    messagingSenderId: '760172319480',
+    measurementId: 'G-60X20TR04J',
+  },
   production: false
 };
 
